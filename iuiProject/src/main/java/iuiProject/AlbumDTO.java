6e4dcbacddc997e696544d86d02a7f0c9f727cb9
RSH <fbtnghks7906@gmail.com>
2023-09-18 17:54:55 +0900

    [add] AlbumDTO, ConnectionPool, MemberDTO, SongDTO, UserCommentDTO

---
+++ iuiProject/src/main/java/iuiProject/AlbumDTO.java
@@ -0,0 +1,108 @@
+package iuiProject;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class AlbumDTO {
+	private int albumId;
+
+	private String albumName;
+
+	private Date releaseDate;
+
+	private String albumType;
+
+	private int numberSongs;
+
+	private String albumIntro;
+
+	private String albumCover;
+
+	public AlbumDTO() {
+		super();
+	}
+
+	public AlbumDTO(int albumId, String albumName, Date releaseDate, String albumType, int numberSongs,
+			String albumIntro, String albumCover) {
+		super();
+		this.albumId = albumId;
+		this.albumName = albumName;
+		this.releaseDate = releaseDate;
+		this.albumType = albumType;
+		this.numberSongs = numberSongs;
+		this.albumIntro = albumIntro;
+		this.albumCover = albumCover;
+	}
+
+	public int getAlbumId() {
+		return albumId;
+	}
+
+	public String getAlbumName() {
+		return albumName;
+	}
+
+	public Date getReleaseDate() {
+		return releaseDate;
+	}
+
+	public String getAlbumType() {
+		return albumType;
+	}
+
+	public int getNumberSongs() {
+		return numberSongs;
+	}
+
+	public String getAlbumIntro() {
+		return albumIntro;
+	}
+
+	public String getAlbumCover() {
+		return albumCover;
+	}
+
+	public void setAlbumId(int albumId) {
+		this.albumId = albumId;
+	}
+
+	public void setAlbumName(String albumName) {
+		this.albumName = albumName;
+	}
+
+	public void setReleaseDate(Date releaseDate) {
+		this.releaseDate = releaseDate;
+	}
+
+	public void setAlbumType(String albumType) {
+		this.albumType = albumType;
+	}
+
+	public void setNumberSongs(int numberSongs) {
+		this.numberSongs = numberSongs;
+	}
+
+	public void setAlbumIntro(String albumIntro) {
+		this.albumIntro = albumIntro;
+	}
+
+	public void setAlbumCover(String albumCover) {
+		this.albumCover = albumCover;
+	}
+
+	/*
+	 * @Override public String toString() { return "AlbumDTO [albumId=" + albumId +
+	 * "]"; }
+	 * 
+	 * @Override public int hashCode() { return Objects.hash(albumCover, albumId,
+	 * albumIntro, albumName, albumType, numberSongs, releaseDate); }
+	 * 
+	 * @Override public boolean equals(Object obj) { if (this == obj) return true;
+	 * if (obj == null) return false; if (getClass() != obj.getClass()) return
+	 * false; AlbumDTO other = (AlbumDTO) obj; return Objects.equals(albumCover,
+	 * other.albumCover) && albumId == other.albumId && Objects.equals(albumIntro,
+	 * other.albumIntro) && Objects.equals(albumName, other.albumName) &&
+	 * Objects.equals(albumType, other.albumType) && numberSongs ==
+	 * other.numberSongs && Objects.equals(releaseDate, other.releaseDate); }
+	 */
+}
---
+++ iuiProject/src/main/java/iuiProject/MemberDTO.java
@@ -0,0 +1,94 @@
+package iuiProject;
+
+import java.util.Objects;
+
+public class MemberDTO {
+	private int memberNo;
+
+	private String id;
+
+	private String pw;
+
+	private String email;
+
+	private String nickname;
+
+	private int status;
+
+	public MemberDTO() {
+		super();
+	}
+
+	public MemberDTO(int memberNo, String id, String pw, String email, String nickname, int status) {
+		super();
+		this.memberNo = memberNo;
+		this.id = id;
+		this.pw = pw;
+		this.email = email;
+		this.nickname = nickname;
+		this.status = status;
+	}
+
+	public int getMemberNo() {
+		return memberNo;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getPw() {
+		return pw;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setMemberNo(int memberNo) {
+		this.memberNo = memberNo;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	/*
+	 * @Override public String toString() { return "MemberDTO [memberNo=" + memberNo
+	 * + ", id=" + id + "]"; }
+	 * 
+	 * @Override public int hashCode() { return Objects.hash(email, id, memberNo,
+	 * nickname, pw, status); }
+	 * 
+	 * @Override public boolean equals(Object obj) { if (this == obj) return true;
+	 * if (obj == null) return false; if (getClass() != obj.getClass()) return
+	 * false; MemberDTO other = (MemberDTO) obj; return Objects.equals(email,
+	 * other.email) && Objects.equals(id, other.id) && memberNo == other.memberNo &&
+	 * Objects.equals(nickname, other.nickname) && Objects.equals(pw, other.pw) &&
+	 * status == other.status; }
+	 */
+}
