95936309ccab20e6450a1784aed4bccad71ff2d6
danbab <danbab2@naver.com>
2023-09-21 13:43:41 +0900

    [temp]

--- iuiProject/src/main/java/iuiProject/MemberDAO.java
+++ iuiProject/src/main/java/iuiProject/MemberDAO.java
@@ -5,15 +5,12 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-
 public class MemberDAO {
 	private String url = "jdbc:oracle:thin:@localhost:1521:xe";
 	private String user = "hr";
 	private String password = "hr";
 	private static final String jdbcclass = "oracle.jdbc.OracleDriver";
 	private ConnectionPool pool;
-	
-	
 
 	public MemberDAO() {
 		super();
@@ -43,33 +40,33 @@ public class MemberDAO {
 	 * catch (Exception e) { e.printStackTrace(); } pstmt.close();
 	 * pool.releaseConnection(conn); return -1; }
 	 */
-		// 회원가입 기능
-		public int join(MemberDTO member) throws SQLException {
-			Connection conn = pool.getConnection();
-			System.out.println("이곳은 DAO "+member.toString());
-			String SQL = "INSERT INTO MEMBER (id, pw, email, nickname, member_no, status) VALUES (?, ?, ?, ?, member_no.nextval, 1)";
-			PreparedStatement pstmt = conn.prepareStatement(SQL);
-			
-				pstmt = conn.prepareStatement(SQL);
-				pstmt.setString(1, member.getId());
-				pstmt.setString(2, member.getPw());
-				pstmt.setString(3, member.getEmail());
-				pstmt.setString(4, member.getNickname());
-				int result = pstmt.executeUpdate();
-			
-			pstmt.close();
-			pool.releaseConnection(conn);
-			return result;
-		}
+	// 회원가입 기능
+	public int join(MemberDTO member) throws SQLException {
+		Connection conn = pool.getConnection();
+		System.out.println("이곳은 DAO " + member.toString());
+		String SQL = "INSERT INTO member (id, pw, email, nickname, member_no, status) VALUES (?, ?, ?, ?, member_no.nextval, 1)";
+		PreparedStatement pstmt = conn.prepareStatement(SQL);
+
+		pstmt = conn.prepareStatement(SQL);
+		pstmt.setString(1, member.getId());
+		pstmt.setString(2, member.getPw());
+		pstmt.setString(3, member.getEmail());
+		pstmt.setString(4, member.getNickname());
+		int result = pstmt.executeUpdate();
+
+		pstmt.close();
+		pool.releaseConnection(conn);
+		return result;
+	}
 
 	// 회원 로그인 기능
 	public int login(String id, String pw) throws SQLException, ClassNotFoundException {
 		Connection conn = pool.getConnection();
-		String SQL = "SELECT pw FROM MEMBER WHERE id = ?";
-		
-		int result=0;
+		String sql = "SELECT pw FROM member WHERE id = ?";
+
+		int result = 0;
 		try {
-			PreparedStatement pstmt = conn.prepareStatement(SQL);
+			PreparedStatement pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, id);
 			ResultSet rs = pstmt.executeQuery();
 
@@ -90,36 +87,55 @@ public class MemberDAO {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		pool.releaseConnection(conn);
-		return result; // 데이터베이스 오류
-	}
-	
-	//ID,PW 매칭 후 블랙리스트인 사람이면 거부
-	public MemberDTO select(String id, String pw) throws SQLException, ClassNotFoundException {
-	    Connection conn = pool.getConnection();
-	    String sql = "select * from member where id=? and pw=?";
-	    PreparedStatement pstmt = conn.prepareStatement(sql);
-	    pstmt.setString(1, id);
-	    pstmt.setString(2, pw);
-	    MemberDTO member = null;
-	    ResultSet rs = pstmt.executeQuery(); // executeQuery 메서드 인자 제거
-	    while (rs.next()) {
-	    	if (rs.getInt("status") > 0) {
-	            member = new MemberDTO(rs.getInt("member_no"), rs.getString("id"), rs.getString("pw"), rs.getString("email"), 
-	                rs.getString("nickname"), rs.getInt("status"));
-	        } else {
-	            // 이 사람은 블랙리스트임
-	            member = null;
-	        }
-	    }
-	    System.out.println(member);
-	    rs.close();
-	    pstmt.close();
-	    pool.releaseConnection(conn);
-	    return member;
+		return result;
 	}
 
+	// ID,PW 매칭 후 블랙리스트인 사람이면 거부
+	public MemberDTO select(String id, String pw) throws SQLException, ClassNotFoundException {
+		Connection conn = pool.getConnection();
+		String sql = "SELECT * FROM member WHERE id=? and pw=?";
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		pstmt.setString(1, id);
+		pstmt.setString(2, pw);
+		MemberDTO member = null;
+		ResultSet rs = pstmt.executeQuery(); // executeQuery 메서드 인자 제거
+		while (rs.next()) {
+			if (rs.getInt("status") > 0) {
+				member = new MemberDTO(
+						rs.getInt("member_no"),
+						rs.getString("id"),
+						rs.getString("pw"),
+						rs.getString("email"),
+						rs.getString("nickname"),
+						rs.getInt("status")
+						);
+			} else {
+				// 이 사람은 블랙리스트임
+				member = null;
+			}
+		}
+		System.out.println(member);
+		rs.close();
+		pstmt.close();
+		pool.releaseConnection(conn);
+		return member;
+	}
 	
-	
+	//회원정보 수정
+	public int updateMemberInfo(MemberDTO member) throws SQLException {
+		Connection conn = pool.getConnection();
+		String sql = "UPDATE member SET pw = ?, nickname = ?, email = ? WHERE member_no = ?";
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		pstmt.setString(1, member.getPw());
+		pstmt.setString(2, member.getNickname());
+		pstmt.setString(3, member.getEmail());
+		pstmt.setInt(4, member.getMemberNo());
+		
+		int result = pstmt.executeUpdate(sql);
+		pstmt.close();
+		pool.releaseConnection(conn);
+		return result;
+	}
 }
---
+++ iuiProject/src/main/webapp/modInfo.jsp
@@ -0,0 +1,49 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ page import="iuiProject.*,java.sql.*"%>
+<jsp:useBean id="service" type="iuiProject.MemberDAO" scope="application"/>
+<jsp:useBean id="member" type="iuiProject.MemberDTO" scope="session"/>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>회원정보 수정</title>
+<!-- 외부 CSS 파일 링크 -->
+<link rel="stylesheet" type="text/css" href="styles.css">
+
+</head>
+<body>
+	<header>
+		<div>
+			<img src="logo.jpg" width="200" height="100">
+		</div>
+	</header>
+	<div class="modifyInfo-container">
+		<h2>회원정보 수정</h2>
+		<form class="modifyInfo-form" action="modInfoAction.jsp" method="POST">
+			<label for="id">아이디:</label> 
+			<input type="text" id="id" name="id" placeholder="아이디" 
+			value="<%= member.getId()%>" readonly required>
+			
+			<label for="currentpw">기존 비밀번호:</label>
+			<input type="password" id="currentpw" name="currentpw" placeholder="기존 비밀번호" 
+			value="<%= member.getPw()%>" readonly required>
+			
+			<label for="pw">새 비밀번호:</label> 
+			<input type="password" id="newpw" name="newpw" placeholder="변경할 비밀번호" required>
+			 
+			<label for="nickname">닉네임:</label> 
+			<input type="text" id="nickname" name="nickname" placeholder="닉네임" 
+			value="<%= member.getNickname()%>" required>
+			 
+			<label for="email">이메일:</label> 
+			<input type="email" id="email" name="email" placeholder="이메일" 
+			value="<%= member.getEmail()%>"><br>
+			
+			<button type="submit">수정</button><br>
+			<button type="reset">초기화</button>
+		</form>
+	</div>
+</body>
+</html>
